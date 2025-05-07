57cddd1aaf77bad5be5c967cecb93bdcadd65ddc
aryalmahesh775 <aryalmahesh775@gmail.com>
2025-05-07 21:11:21 +0545

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const {sequelize} = require("./models")
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+async function connectToDatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to the database has been established successfully.");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+}
+
+app.listen(port, async () => {
+  await connectToDatabase();
   console.log(`Server is running on port ${port}`);
 });
