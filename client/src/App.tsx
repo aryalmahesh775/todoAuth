402e4a2b6d9f04f3da56a4590a694d6c43a0574e
mahesharyal775 <mahesharyal@reluconsultancy.in>
2025-05-07 16:55:51 +0545

    Initial commit with root-level .gitignore , client and server setUp

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <p className="text-red-900 bg-blue-100">This is a typescript project</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
