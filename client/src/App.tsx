bfb8231c5558faaca11e00099ccbd0b3247537d3
aryalmahesh775 <aryalmahesh775@gmail.com>
2025-05-07 18:41:54 +0545

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="">
-        <p className="text-red-900 bg-blue-100">This is a typescript project</p>
+        <p className="text-red-900 bg-blue-100">This is a typescript project after updating the code</p>
       </div>
     </>
   );
