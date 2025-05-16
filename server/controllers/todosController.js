b3d24f976417e03e72d9e6658183323833f0341c
aryalmahesh775 <aryalmahesh775@gmail.com>
2025-05-16 20:53:41 +0545

    get update and delete todo BE

--- server/controllers/todosController.js
+++ server/controllers/todosController.js
@@ -24,6 +24,66 @@ const createTodos = async(req, res) => {
     }
 }
 
+//get all todos by userId
+const getTodosByUserId = async(req,res) =>{
+    try {
+        const userId= req.user.userId;
+        const todos= await model.Todo.findAll({
+            where:{userId}
+        })
+        return responde(res,200, "Todos get successfully",todos)
+    } catch (error) {
+        console.error("Error getting todos",error);
+        return responde(res,500,"something went wrong")
+    }
+}
+
+//update by id
+const updateTodo = async(req,res) =>{
+    try {
+        const userId = req.user.userId;
+        const {id} = req.params;
+        const {title,description,dueDate,isCompleted}= req.body;
+
+        const todo = await model.Todo.findOne({
+            where: {id,userId}
+        })
+        if(!todo) return responde(res,400,"Todo not found by this id")
+        
+            todo.title= title || todo.title
+            todo.description= description || todo.description;
+            todo.dueDate= dueDate || todo.dueDate;
+            todo.isCompleted = isCompleted || todo.isCompleted
+
+            await todo.save();
+            return responde(res,200,"Todo updated successfully",todo)
+    } catch (error) {
+        console.error("error while updating todo",error)
+        return responde(res,500, "something went wrong")
+    }
+}
+const deleteTodoByUserId = async(req,res) =>{
+     try {
+        const userId = req.user.userId;
+        const {id} = req.params;
+       
+        const todo = await model.Todo.findOne({
+            where: {id,userId}
+        })
+        if(!todo) return responde(res,400,"Todo not found by this id")
+
+            await todo.destroy()
+
+          return responde(res,200,"Todo deleted successfully")
+     } catch (error) {
+                console.error("error while delete todo",error)
+        return responde(res,500, "something went wrong")
+     }
+}
+
 module.exports = {
-    createTodos
+    createTodos,
+    getTodosByUserId,
+    updateTodo,
+    deleteTodoByUserId
 }
--- server/routes/todoRoute.js
+++ server/routes/todoRoute.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const todosController = require('../controllers/todosController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// router.post("/auth/signup", authController.signup)
-
-
 // auth request
 router.post("/user/todos", authMiddleware, todosController.createTodos);
+router.get("/user/todos", authMiddleware, todosController.getTodosByUserId);
+router.put("/user/todos/:id", authMiddleware, todosController.updateTodo);
+router.delete("/user/todos/:id", authMiddleware, todosController.deleteTodoByUserId);
 
 module.exports = router;
